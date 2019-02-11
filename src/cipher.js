7adf82eddbe6d2b8410fba94d91c06f71f2e90ec
“DanielaEstrada” <dani.strada@gmail.com>
2019-02-11 18:31:05 -0300

    orden

--- src/cipher.js
+++ src/cipher.js
@@ -20,6 +20,7 @@ window.cipher = {
             if ((charAscii >= 65) && (charAscii <= 90)){
               ascii = ((charAscii - 65 + offset) % 26 + 65);
               code1 = String.fromCharCode(ascii);}
+              
             // Lowercase letters
             if ((charAscii >= 97) && (charAscii <= 122)){
               ascii = ((charAscii - 97 + offset) % 26 + 97);
@@ -39,21 +40,21 @@ window.cipher = {
 
                     result += code1;
                   }
-        //fin
-        return result;
-      },
 
-      decode: (str, offset) => {
+                  return result;
+                },
 
-        let caesarShift2
-        let ascii2;
-        let result2 = "";
-        let code2;
+                decode: (str, offset) => {
 
-        if (offset < 0)
-          return caesarShift2(str, offset + 26);
+                  let caesarShift2
+                  let ascii2;
+                  let result2 = "";
+                  let code2;
 
-        for (let i = 0; i < str.length; i ++) {
+                  if (offset < 0)
+                    return caesarShift2(str, offset + 26);
+
+                  for (let i = 0; i < str.length; i ++) {
 
             // Aplica fórmula ascii
             let charAscii2 = str.charCodeAt(i);
@@ -64,25 +65,23 @@ window.cipher = {
             ascii2=((charAscii2 - 90 - offset) % 26 + 90);
             code2=String.fromCharCode(ascii2);}
 
-            // Lowercase letters
-            if ((charAscii2 >= 97) && (charAscii2 <= 122)){
-              ascii2=((charAscii2 - 122 - offset) % 26 + 122);
-              code2=String.fromCharCode(ascii2);}
+// Lowercase letters
+if ((charAscii2 >= 97) && (charAscii2 <= 122)){
+  ascii2=((charAscii2 - 122 - offset) % 26 + 122);
+  code2=String.fromCharCode(ascii2);}
 
+// Numbers (0-9)
+if ((charAscii2 >= 48) && (charAscii2 <= 57)) {
+  ascii2 = ((charAscii2 - 57 - offset) % 10 + 57);
+  code2 = String.fromCharCode(ascii2);}
 
-            // Numbers (0-9)
-            if ((charAscii2 >= 48) && (charAscii2 <= 57)) {
-              ascii2 = ((charAscii2 - 57 - offset) % 10 + 57);
-              code2 = String.fromCharCode(ascii2);}
+// Special Characters
+if ((charAscii2 >= 32) && (charAscii2 <= 47)) {
+  ascii2 = ((charAscii2 - 47 - offset) % 16 + 47);
+  code2 = String.fromCharCode(ascii2);}
 
-
-                  // Special Characters
-                  if ((charAscii2 >= 32) && (charAscii2 <= 47)) {
-                    ascii2 = ((charAscii2 - 47 - offset) % 16 + 47);
-                    code2 = String.fromCharCode(ascii2);}
-
-                    result2 += code2;
-                  }
-                  return result2;
-                }
-              };
+  result2 += code2;
+}
+return result2;
+}
+};
